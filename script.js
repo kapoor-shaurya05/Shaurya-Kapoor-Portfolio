5586338ebdeb32937e7219ea9488df891151383e
Shaurya Kapoor <shauryakapoor.work@gmail.com>
2026-02-14 20:30:56 +0530

    changed the picture & scrollbar colour

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Smooth scrolling
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-                const target = document.querySelector(this.getAttribute('href'));
-                if (target) {
-                    target.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'start'
-                    });
-                }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
             });
-        });
+        }
+    });
+});
+
+//scrollbar color change 
+window.addEventListener("scroll",function(){
+    if(window.scrollY > 100){
+        document.body.classList.add("scrolled");
+    }else{
+        document.body.classList.remove("scrolled");
+    }
+});
